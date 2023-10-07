8bb7b01426ab3eb53f64cc4c4e81173f97cf7b1d
adrianplacintadev <placinta.adrianstefan@gmail.com>
2023-10-07 20:22:22 +0300

    ContainsDuplicate

---
+++ ContainsDuplicate.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+/*
+Link:
+    https://leetcode.com/problems/contains-duplicate/submissions/
+*/
+class Solution
+{
+public:
+    bool containsDuplicate(std::vector<int> &nums)
+    {
+        std::unordered_map<int, int> map;
+        for (const auto &x : nums)
+        {
+            if (map[x] == 1)
+                return true;
+            else
+                map[x] = 1;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    std::vector<int> x = {1, 2, 3, 3, 4};
+    sol.containsDuplicate(x);
+}
